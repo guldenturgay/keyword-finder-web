73bca2776871a1f64c236277a91d3f212b71d406
Guldensle <guldenturgay@outlook.com>
2021-06-17 17:23:22 -0700

    removed host

--- app.py
+++ app.py
@@ -29,5 +29,5 @@ def find_words():
 if __name__ == '__main__':
     
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', debug=True, port=port)
+    app.run(debug=True, port=port)
     
